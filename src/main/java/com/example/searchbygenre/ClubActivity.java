d6dbcd13bc2f8a584825faaa72dfb76bd08f937a
Chisom Okwor <chiggyokwor@gmail.com>
2020-09-14 12:42:54 -0500

    Implemented persistence

--- src/main/java/com/example/searchbygenre/ClubActivity.java
+++ src/main/java/com/example/searchbygenre/ClubActivity.java
@@ -1,13 +1,131 @@
 package com.example.searchbygenre;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.*;
+
+import static android.R.layout.simple_list_item_1;
 
 public class ClubActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
+        myDb = new DatabaseHelper(this);
+
+        String clubName = getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT);
+        Log.i("ClubActivity", "ClubNAME " + clubName);
+        getSupportActionBar().setTitle(clubName);
+
+        final Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+        final ListView listView = (ListView) findViewById(R.id.bookList);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(ClubActivity.this, simple_list_item_1,
+                getResources().getStringArray(R.array.Genre));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final Hashtable<String, String> my_dict = new Hashtable<String, String>();
+        my_dict.put("Fiction", "fiction.json?");
+        my_dict.put("Biographies", "biography.json?");
+        my_dict.put("Mental Health", "mental_health.json?");
+        my_dict.put("Romance", "romance.json?");
+        my_dict.put("Children", "juvenile_fiction.json?");
+        my_dict.put("Science", "science.json?");
+        my_dict.put("History", "history.json?");
+        my_dict.put("Select a Genre", "none");
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String text = mySpinner.getSelectedItem().toString();
+
+                String apiTitle = my_dict.get(text);
+                Log.i("Dropdown api Response", apiTitle);
+                Log.i("Dropdown Response", text);
+
+                String URL = "http://openlibrary.org/subjects/" + apiTitle;
+
+                if (apiTitle == "none") {
+                    URL = "http://openlibrary.org/subjects/horror.json?published_in=1500-1600";
+                } else {
+                    textView.setText("Results");
+                }
+                JsonObjectRequest objectRequest = new JsonObjectRequest(
+                        Request.Method.GET,
+                        URL,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                JSONArray works = null;
+                                List<String> mylist = null;
+                                try {
+                                    works = response.getJSONArray("works");
+                                    mylist = new ArrayList<String>();
+
+                                    for (int i = 0; i < works.length(); ++i) {
+                                        JSONObject work = works.getJSONObject(i);
+                                        String title = work.getString("title");
+                                        mylist.add(title);
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                                        (ClubActivity.this, android.R.layout.simple_list_item_1, mylist);
+
+                                // DataBind ListView with items from ArrayAdapter
+                                listView.setAdapter(arrayAdapter);
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("Rest Response", error.toString());
+                            }
+                        }
+                );
+
+                requestQueue.add(objectRequest);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+
     }
 }
--- src/main/java/com/example/searchbygenre/MainActivity.java
+++ src/main/java/com/example/searchbygenre/MainActivity.java
@@ -1,122 +1,161 @@
 package com.example.searchbygenre;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.VoiceInteractor;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonObject;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.*;
-
-import static android.R.layout.simple_list_item_1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KEY_ITEM_TEXT = "item_text";
+    public static final String KEY_ITEM_POSITION = "item_position";
+    public static final int EDIT_TEXT_CODE = 20;
+    List<String> items =  new ArrayList<>();
+    Button startClubBtn;
+    Button joinClubBtn;
+    Button createBtn;
+    EditText etClubName;
+    RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
+    TextView myClubz;
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
 
-        final Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
-        final ListView listView = (ListView) findViewById(R.id.bookList);
-        final TextView textView = (TextView) findViewById (R.id.textView);
+//      myClubz = findViewById(R.id.myClubz);
+//      startClubBtn = findViewById(R.id.sta rtClubBtn);
+        createBtn = findViewById(R.id.createBtn);
+//      joinClubBtn = findViewById(R.id.joinClubBtn);
+        etClubName = findViewById(R.id.etClubName);
+        rvItems = findViewById(R.id.rvItems);
+        loadItems();
 
-        ArrayAdapter <String> myAdapter = new ArrayAdapter<String>(MainActivity.this, simple_list_item_1,
-                getResources().getStringArray(R.array.Genre));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
-
-        final RequestQueue requestQueue = Volley.newRequestQueue(this);
-        final Hashtable<String, String> my_dict = new Hashtable<String, String>();
-        my_dict.put("Fiction", "fiction.json?");
-        my_dict.put("Biographies", "biography.json?");
-        my_dict.put("Mental Health", "mental_health.json?");
-        my_dict.put("Romance", "romance.json?");
-        my_dict.put("Children", "juvenile_fiction.json?");
-        my_dict.put("Science", "science.json?");
-        my_dict.put("History", "history.json?");
-        my_dict.put("Select a Genre", "none");
-
-        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
             @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                String text = mySpinner.getSelectedItem().toString();
-                String apiTitle = my_dict.get(text);
-                Log.i("Dropdown api Response", apiTitle);
-                Log.i("Dropdown Response", text);
+            public void onItemLongClicked(int position) {
+                // Delete the item from the model
+                items.remove(position);
+                // Notify the adapter
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item removed ", Toast.LENGTH_SHORT).show();
+                Integer deletedRow = myDb.deleteData(etClubName.getText().toString());
+                if (deletedRow > 0)
 
-                String URL = "http://openlibrary.org/subjects/" + apiTitle;
-
-                if(apiTitle == "none") {
-                    URL = "http://openlibrary.org/subjects/horror.json?published_in=1500-1600";
-                }
-                else{
-                    textView.setText("Results");
-                }
-                JsonObjectRequest objectRequest = new JsonObjectRequest(
-                        Request.Method.GET,
-                        URL,
-                        null,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                JSONArray works = null;
-                                List<String> mylist = null;
-                                try {
-                                    works = response.getJSONArray("works");
-                                    mylist = new ArrayList<String>();
-
-                                    for (int i = 0; i < works.length(); ++i) {
-                                        JSONObject work = works.getJSONObject(i);
-                                        String title = work.getString("title");
-                                        mylist.add(title);
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                                        (MainActivity.this, android.R.layout.simple_list_item_1, mylist);
-
-                                // DataBind ListView with items from ArrayAdapter
-                                listView.setAdapter(arrayAdapter);
-
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.e("Rest Response", error.toString());
-                            }
-                        }
-                );
-
-                requestQueue.add(objectRequest);
+                    Toast.makeText(getApplicationContext(), "Data Deleted ", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Data Deleted ", Toast.LENGTH_SHORT).show();
             }
+        };
+
+        ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
+            public void onItemClicked(int position) {
+                Log.d("MainActivity", "Single click at position " + position);
+                // Create the new Activity
+                Intent i = new Intent(MainActivity.this,ClubActivity.class);
+                // Pass the data being edited
+                i.putExtra(KEY_ITEM_TEXT, items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+                // Display the Activity
+                startActivityForResult(i, EDIT_TEXT_CODE);
             }
+        };
 
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener, onClickListener);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+//        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+//            @Override
+//            public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+//                return 0;
+//            }
+//
+//            @Override
+//            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+//                return false;
+//            }
+//
+//            @Override
+//            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+//                    removeItem(() viewHolder.itemView.getTag());
+//            }
+//        });
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = etClubName.getText().toString();
+                items.add(todoItem);
+                // Notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() -1);
+                etClubName.setText("");
+              //  Toast.makeText(getApplicationContext(), "Item added", Toast.LENGTH_SHORT).show();
+                saveItems(todoItem);
+            }
         });
+    }
+
+    // handle the result of the edit Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (RESULT_OK == resultCode && requestCode == EDIT_TEXT_CODE) {
+            // Retrieve the updated text value
+            String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+            // extract the original position of the edited item from the position key
+            int position = data.getExtras().getInt(KEY_ITEM_POSITION);
+
+            // Update the model at the right position with a new item
+            items.set(position, itemText);
+            // Notify the adapter
+            itemsAdapter.notifyItemChanged(position);
+            // Persist the changes
+            Toast.makeText(getApplicationContext(), "Item updated!", Toast.LENGTH_SHORT).show();
+        } else {
+            Log.w("MainActivity", "Unknown call to onActivityResult");
+        }
+    }
+
+    public void loadItems() {
+        Cursor result = myDb.getAllData();
+        if (result.getCount() == 0){
+            // show message
+            return;
+        }
+      //  StringBuffer buffer = new StringBuffer();
+        while(result.moveToNext()){
+            items.add(result.getString(1));
+            //buffer.append("ID : " + result.getString(0) + "\n");
+            //buffer.append("Club Name : " + result.getString(1) + "\n\n");
+        }
+    }
 
 
+    private void saveItems(String Item){
+        // Add item to the Database
+        boolean isInserted = myDb.insertData(Item);
+        if(isInserted == true)
+            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_LONG);
     }
 }
